a06906aad8b8117827c8e87b1ac854b39ce50608
hzy <821607617@qq.com>
2019-06-22 22:20:33 +0800

    更新login，存入session

--- application/index/controller/Login.php
+++ application/index/controller/Login.php
@@ -1,11 +1,35 @@
 <?php
 namespace app\index\controller;
 use think\Controller;
+use think\captcha\Captcha;
+use Request;
+use think\facade\Session;
 class Login extends Controller
 {
     //登录
     function login(){
+        $arr=db('user')->find();
+        $this->assign('arr',$arr);
         return $this->fetch('login');
+    }
+    //登录验证，跳转
+    function loginAction(){
+        $code=Request::post('code');
+        $user=Request::post('user');
+        $pwd=Request::post('pwd');
+        $res=db('user')->find();
+        if( !captcha_check($code ))
+        {
+            $arr=['code'=>'1','status'=>'error','message'=>'验证码错误'];
+        }elseif ($user!=$res['user_name'] || $pwd!=$res['password']){
+            $arr=['code'=>'2','status'=>'error','message'=>'用户名或密码错误'];
+        }else{
+            Session::set('name',$res['user_name']);
+            Session::set('id',$res['id']);
+            $arr=['code'=>'0','status'=>'success','message'=>''];
+        }
+        $json=json_encode($arr);
+        echo $json;
     }
     //注册
     function register(){
@@ -24,6 +48,7 @@ class Login extends Controller
         return $this->fetch('mima');
     }
     function out(){
-        $this->success('退出成功','login/login');
+        Session::delete('name');
+        $this->register('login/login');
     }
 }
